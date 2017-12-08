769d1c9139547c2bb9a1f05c49fce8d642775056
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-12-08 14:54:56 -0500

    Updated from Brython Server: 12/8/2017 2:54:55 PM

--- findWord.py
+++ findWord.py
@@ -6,17 +6,24 @@ dictionary = open('engmix.txt')
 word = input('Enter an word:')
 
 
-counter = 0
-
 def wordAsk():
+    counter = 0
 
     for words in dictionary:
         if words.strip() == word:
-            print('is in the dictionary')
+            print(word, 'is in the dictionary')
             counter += 1
             break
 
     if counter == 0:
-        print('is not the dictionary')
+        print(word, 'is not the dictionary')
 
 print(wordAsk())
+
+num = int(input('Enter the numbered word you like to look for'))
+
+dictList = []
+
+for word in dictionary:
+    dictList.append(word)
+    print(dictList[-1])
