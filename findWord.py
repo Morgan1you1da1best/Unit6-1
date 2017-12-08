1724001846b0573131bbb4b1620febabc602a21c
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-12-08 14:42:39 -0500

    Updated from Brython Server: 12/8/2017 2:42:38 PM

--- findWord.py
+++ findWord.py
@@ -1,1 +1,22 @@
+#Morgan Baughman
+#12/08/17
+#find word in dictionary
 
+dictionary = open('engmix.txt')
+word = input('Enter an word:')
+
+
+counter = 0
+
+def wordAsk():
+
+    for words in dictionary:
+        if words.strip() == word:
+            print('is in the dictionary')
+            counter += 1
+            break
+
+    if counter == 0:
+        print('is not the dictionary')
+
+print(wordAsk())
