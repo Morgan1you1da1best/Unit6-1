63c62b607eb3e557380294987f5add844240620b
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-12-07 23:59:20 -0500

    Updated from Brython Server: 12/7/2017 11:59:18 PM

--- longestWord.py
+++ longestWord.py
@@ -1,1 +1,17 @@
+#Morgan Baughman
+#12/6/17
+#fileDemo.py - how to read a file
 
+dictionary = open('engmix.txt')
+
+longest = 0
+word = ''
+
+for words in dictionary:
+    length = len(word)
+    if length > longest:
+        lenght = longest
+        words = word
+    
+    
+print('The longest word is', word)
