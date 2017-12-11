125bf2d677ddeedd187bc3168fe528d3d145fbd2
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-12-10 23:14:47 -0500

    Updated from Brython Server: 12/10/2017 11:14:46 PM

--- findWord.py
+++ findWord.py
@@ -14,16 +14,34 @@ def wordAsk():
             print(word, 'is in the dictionary')
             counter += 1
             break
-
     if counter == 0:
         print(word, 'is not the dictionary')
-
+        
 print(wordAsk())
 
-num = int(input('Enter the numbered word you like to look for'))
 
+num = int(input('Enter the numbered word you like to look for'))
 dictList = []
 
 for word in dictionary:
     dictList.append(word)
-    print(dictList[-1])
+    print(dictList[num-1])
+    
+
+def exclaimationPoint():
+    file = open("longestWord.py")
+    for line in file:
+        print(line.strip()+"!")
+        
+
+def letterSearch():
+    letter = input("Enter a letter: ")
+    
+    letterCount = 0
+    word = ""
+    for words in dictionary:
+        loopCount = words.count(letter)
+        if loopCount > letterCount:
+            letterCount = loopCount
+            word = words
+    print("The word with the most", letter+"s is", word)
