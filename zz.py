25afc2dd4cd758dc13664640335e0beccda3c586
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-12-11 14:53:26 -0500

    Updated from Brython Server: 12/11/2017 2:53:25 PM

--- zz.py
+++ zz.py
@@ -1,1 +1,10 @@
+#Morgan Baughman
+#12/11/17
+#zz.py - Find a word in the dictionary that contains zz. 
 
+dictionary = open('engmix.txt')
+
+for word in dictionary:
+    word = word.strip()
+    if 'zz' in word:
+        print(word)
