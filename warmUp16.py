901a1fbd665c0bc4e31bcda9958a34ba21e2d3d7
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-12-11 14:45:07 -0500

    Updated from Brython Server: 12/11/2017 2:45:06 PM

--- warmUp16.py
+++ warmUp16.py
@@ -1,1 +1,14 @@
+#Morgan Baughman
+#12/11/17
+#warmUp16.py - find all the words in the dictionary taht start with the first initial and last intional of your name in it.
+
+dictionary = open('engmix.txt')
+
+for word in dictionary:
+    word = word.strip()
+    if len(word) > 0:
+        if word[0] == 'm' and word[-1] == 'b':
+            print(word)
+
+
 
